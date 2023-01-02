ccd299516f7098645b5e73e8a29abe2f6eb8391d
Asfand <Asfandkhan404@gmail.com>
2023-01-02 17:08:32 +0500

    Updated Firebase config

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,154 +1,130 @@
+var ADD_HCP = false
+var EDIT_HCP = false
 
-var ADD_HCP = false;
-var EDIT_HCP = false;
+function loaded () {
+  if (!checkedLoggedIn()) {
+    $('#N6s45nqg5mKU6vIcDwNvzCizUL9B1lc6').removeClass('hide')
+    return
+  }
+  setMaxDate()
+  showDashboardAndGetData()
+}
 
+function showDashboardAndGetData () {
+  $('#N6s45nqg5mKU6vIcDwNvzCizUL9B1lc6').addClass('hide')
+  $('#N6s45nqg5mKU6vIcDwNvzCizUL9B1lc5').removeClass('hide')
 
-function loaded() {
-    if (!checkedLoggedIn()) {
-        $('#N6s45nqg5mKU6vIcDwNvzCizUL9B1lc6').removeClass('hide');
-        return;
+  showUserData()
+}
+
+function checkedLoggedIn () {
+  var googleName = localStorage.getItem('google-name')
+  return googleName
+}
+
+function logout () {
+  // setTimeout(function(){
+  localStorage.clear()
+  window.location.reload()
+  // }, 2000);
+}
+
+function deleteHCP () {
+  Swal.fire({
+    title: 'You sure you want to delete this HCP?',
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: 'Yes',
+    denyButtonText: `No`
+  }).then(async result => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      $('#delete_hcp_form').submit()
     }
-    setMaxDate();
-    showDashboardAndGetData();
+  })
 }
 
-function showDashboardAndGetData() {
+function logout () {
+  // setTimeout(function(){
+  localStorage.clear()
+  window.location.reload()
+  window.location.href = 'index.php'
 
-    $('#N6s45nqg5mKU6vIcDwNvzCizUL9B1lc6').addClass('hide');
-    $('#N6s45nqg5mKU6vIcDwNvzCizUL9B1lc5').removeClass('hide');
-
-    showUserData();
+  // }, 2000);
 }
 
-function checkedLoggedIn() {
-    var googleName = localStorage.getItem("google-name")
-    return googleName
-}
+function userSubmit () {
+  var email = $('#email').val()
+  var password = $('#password').val()
 
+  // Authentication
+  var provider = new firebase.auth.GoogleAuthProvider()
 
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential
 
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken
+      // The signed-in user info.
+      var user = result.user
 
+      localStorage.setItem('google-signin', true)
+      localStorage.setItem('google-name', user.displayName)
 
-function logout() {
-    // setTimeout(function(){
-    localStorage.clear();
-    window.location.reload();
-    // }, 2000);
-}
+      showDashboardAndGetData()
 
-
-function deleteHCP() {
-    
-    Swal.fire({
-      title: 'You sure you want to delete this HCP?',
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: 'Yes',
-      denyButtonText: `No`,
-    }).then(async (result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-            $("#delete_hcp_form").submit();
-
-        } 
+      // ...
     })
-    
-    
+    .catch(error => {
+      // Handle Errors here.
+      var errorCode = error.code
+      var errorMessage = error.message
+      // The email of the user's account used.
+      var email = error.email
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential
+      // ...
+
+      console.log(error)
+
+      alert(
+        'There is an error signin in with Google method, please try again later.'
+      )
+    })
 }
 
-function logout() {
-    // setTimeout(function(){
-    localStorage.clear();
-    window.location.reload();
-    window.location.href = 'index.php';
-
-    // }, 2000);
+function loaded () {
+  if (!checkedLoggedIn()) {
+    return (window.location.href = 'index.php')
+  }
+  showUserData()
 }
 
-
-function userSubmit() {
-    var email = $("#email").val();
-    var password = $("#password").val();
-
-    // Authentication 
-
-
-    var provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase.auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-            /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
-
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-
-            localStorage.setItem("google-signin", true)
-            localStorage.setItem("google-name", user.displayName)
-
-            showDashboardAndGetData();
-
-
-
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-
-            console.log(error)
-
-
-            alert("There is an error signin in with Google method, please try again later.")
-
-
-        });
-
-
-
-
+function checkedLoggedIn () {
+  var googleName = localStorage.getItem('google-name')
+  return googleName
 }
 
-function loaded() {
-    if (!checkedLoggedIn()) {
-        return window.location.href = 'index.php';
-    }
-    showUserData();
+function showDashboardAndGetData () {
+  window.location.href = 'home.php?ID=1'
+
+  showUserData()
 }
 
-function checkedLoggedIn() {
-    var googleName = localStorage.getItem("google-name")
-    return googleName
-}
-
-
-function showDashboardAndGetData() {
-
-    window.location.href = 'home.php?ID=1';
-
-    showUserData();
-}
-
-function showUserData() {
-
-
-    $(".google-name").text(localStorage.getItem("google-name"))
-    $(".google-name-avatar").attr("src", "https://ui-avatars.com/api/?bold=true&background=0000ff&color=fff&rounded=true&name="+localStorage.getItem("google-name"))
-
-
+function showUserData () {
+  $('.google-name').text(localStorage.getItem('google-name'))
+  $('.google-name-avatar').attr(
+    'src',
+    'https://ui-avatars.com/api/?bold=true&background=0000ff&color=fff&rounded=true&name=' +
+      localStorage.getItem('google-name')
+  )
 }
 
 $('select[data-id="select_hcp_name"').on('change', function (e) {
-    var valueSelected = this.value;
-    window.location.href = 'home.php?ID='+valueSelected;
-}); 
-
-
+  var valueSelected = this.value
+  window.location.href = 'home.php?ID=' + valueSelected
+})
--- assets/js/config.js
+++ assets/js/config.js
@@ -1,30 +1,10 @@
-// ST ->
-
-// Your web app's Firebase configuration
-// var firebaseConfig = {
-//     apiKey: "AIzaSyB6RqNf5JRi4Nj27Jrza93D2KKEW_3oUjA",
-//     authDomain: "vc-hcp1-ac44c.firebaseapp.com",
-//     databaseURL: "https://vc-hcp1-ac44c-default-rtdb.firebaseio.com",
-//     projectId: "vc-hcp1-ac44c",
-//     storageBucket: "vc-hcp1-ac44c.appspot.com",
-//     messagingSenderId: "808747515459",
-//     appId: "1:808747515459:web:c09857c3c888dc8ac21ef6",
-//     measurementId: "G-S1YG22GY8D"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCi9OFJau3_AZzSrTctRQgCXMrDOFHMo0I",
-    authDomain: "test-hcp.firebaseapp.com",
-    projectId: "test-hcp",
-    storageBucket: "test-hcp.appspot.com",
-    messagingSenderId: "130298051766",
-    appId: "1:130298051766:web:acbbd63175cbdbfb5d3a17",
-    measurementId: "G-F6MLCMFLVM"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAc-fdYo1MdSxQ-2bmDKbBpaaQCnJE2LYo",
+  authDomain: "test-hcp-edd3e.firebaseapp.com",
+  projectId: "test-hcp-edd3e",
+  messagingSenderId: "121859541341",
+  appId: "1:121859541341:web:24a2619987bf4bc1d300b4"
+};
 
-
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
